a4b53f3eb5112a26e36269ce48581699e3ec72e8
Swadeep Dhondi <dhondiswadeepdsp6@gmail.com>
2024-10-27 14:00:16 +0530

    herosection updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <>
-      <Navbar/>
+      <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <Hero />
+      </div>
     </>
   );
 }
