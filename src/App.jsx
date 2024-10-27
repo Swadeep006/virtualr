c136f8aac2f4fbe8c3658884079b6e9fc798abd9
Swadeep Dhondi <dhondiswadeepdsp6@gmail.com>
2024-10-27 16:01:30 +0530

    workflowsection updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import FeatureSection from "./components/FeatureSection";
+import WorkFlow from "./components/WorkFlow";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className="max-w-7xl mx-auto pt-20 px-6">
         <Hero />
         <FeatureSection/>
+        <WorkFlow/>
       </div>
     </>
   );
